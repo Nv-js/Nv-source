dfd737b0c51b5f3b0a98c880c4db1fb6feb18db4
dengyingyi2 <dengyingyi@jd.com>
2023-03-11 14:46:52 +0800

    dev

--- src/js/inner/input.js
+++ src/js/inner/input.js
@@ -32,24 +32,40 @@ var _input = {
                     // 初始化删除按钮
                     if ($n.hasClass(opt.options.clearClassName)) {
                         $n.wrap('<div class="' + opt.options.groupClassName + '"></div>');    // add group element
-                        $n.after('<i class="' + opt.options.iconCloseCircle + '"></i>');  // add clean icon
+                        $n.after('<i style="cursor:pointer" class="' + opt.options.iconCloseCircle + '"></i>');  // add clean icon
 
                         var selscter = $n.closest(opt.options.groupSelector).children("." + opt.options.iconCloseCircle);
                         selscter.data(_input.options.cacheName, _input.options._obj);
 
+                        var $group = $n.closest(opt.options.groupSelector);
+                        
+                        $group.off('mouseenter').on('mouseenter', _input.eventFn.groupEnter);
+                        $group.off('mouseleave').on('mouseleave', _input.eventFn.groupLeave);
+                        $group.off('click').on('click', '.nvicon-close-circle', _input.eventFn.groupClick); //委托
+                        $n.off('focus').on('focus', _input.eventFn.inputFocus);
+                        $n.off('blur').on('blur', _input.eventFn.inputBlur);
+                        $n.off("input propertychange").on("input propertychange", _input.eventFn.inputChange);
                         // 监听input变化
                         // todo BUG：dom插入数据不触发
-                        $n.off("input propertychange").on("input propertychange", _input.eventFn.showIconFn);
+                        // $n.off("input propertychange").on("input propertychange", _input.eventFn.showIconFn);
                         // 初始化默认值
-                        if ($n.val().length > 0) {
-                            $n.closest(opt.options.groupSelector).children('.' + opt.options.iconCloseCircle).show();
-                        }
-
+                        // if ($n.val().length > 0) {
+                        //     $n.closest(opt.options.groupSelector).children('.' + opt.options.iconCloseCircle).show();
+                        // }
                         // 监听清理图标点击事件
                         selscter.off("click").on("click", _input.eventFn.clearFn);
                     }
                 }
-
+                // 解决动态赋值，无法触发change事件
+                // var o = $.fn.val;
+                // $.fn.val = function() {
+                //     var r = o.apply(this, arguments);
+                //     if (this.is(n) && arguments.length > 0) {
+                //         console.log(1234, n)
+                //         this.trigger("input");
+                //     }
+                //     return r;
+                // }
                 // 去除inline-block时，空格造成的间距
                 if ($n.parent().hasClass(opt.options.wapperClassName)) {
                     $n.parent().addClass('removeTextNodes');
@@ -95,14 +111,68 @@ var _input = {
                 opts.afterFn.call(this, opts);
             }
         },
+        groupClick: function () {
+            // 清除逻辑的健壮处理
+            var $group = $(this),
+                $i = $group.children('i');
+            $i.hide().css('opacity', 0);
+        },
+        inputChange: function () {
+            var $n = $(this),
+                $i = $n.siblings('i'),
+                isFocus = $n.is(":focus");  
+            //非焦点态
+            if(!isFocus) {
+                $i.hide().css('opacity', 0);
+            }else {
+                if ($n.val().length > 0) {
+                    $i.show().css('opacity', 0.25);
+                }else {
+                    $i.hide().css('opacity', 0);
+                }
+            }
+        },
+        inputBlur: function () {
+            var $n = $(this),
+                $i = $n.siblings('i');
+            $i.css('opacity', 0); //仅隐藏
+        },
+        inputFocus: function () {
+            var $n = $(this),
+                $i = $n.siblings('i');
+            if ($n.val().length > 0) {
+                $i.show().css('opacity', 0.25);
+            }
+        },
+        groupEnter: function () {
+            /**
+             * nv-input-group
+             * nv-input nv-input-clear
+             * nvicon-close-circle
+             */
+            var $group = $(this),
+                $n = $group.children('input'),
+                $i = $group.children('i');
+            if ($n.val().length > 0) {
+                $i.show().css('opacity', 0.25);
+            }
+        },
+        groupLeave: function () {
+            var $group = $(this),
+                $n = $group.children('input'),
+                $i = $group.children('i'),
+                isFocus = $n.is(":focus");  
+            if(!isFocus) {
+                $i.hide().css('opacity', 0.25);
+            }
+        },
         // 点击清理图标
         clearFn: function () {
             var $this = $(this),
                 _obj = $this.data(_input.options.cacheName);
-            $this.closest(_obj.options.groupSelector).children(_obj.selector).val('');
-            $this.hide();
+            $this.closest(_obj.options.groupSelector).children(_obj.selector).val('').focus();
+            $this.hide().css('opacity', 0);
         },
-
         // 显示清空图标
         showIconFn: function () {
             var $this = $(this),
